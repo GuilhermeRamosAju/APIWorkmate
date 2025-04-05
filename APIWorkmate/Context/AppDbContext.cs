404a388826fd14c0c06a7b120bf0a115fe6af7b3
Guilherme Ramos <105815487+GuilhermeRamosAju@users.noreply.github.com>
2025-04-04 23:46:04 -0300

    Criando DbContext e primeira Migration

---
+++ APIWorkmate/Context/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using APIWorkmate.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIWorkmate.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Avaliacao> Avaliacoes { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Contratacao> Contratacoes { get; set; }
+    public DbSet<Mensagem> Mensagens { get; set; }
+    public DbSet<Servico> Servicios { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Mensagem>()
+            .HasOne(m => m.Remetente)
+            .WithMany(u => u.MensagensEnviadas)
+            .HasForeignKey(m => m.RemetenteId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Mensagem>()
+            .HasOne(m => m.Destinatario)
+            .WithMany(u => u.MensagensRecebidas)
+            .HasForeignKey(m => m.DestinatarioId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
--- APIWorkmate/Program.cs
+++ APIWorkmate/Program.cs
@@ -1,9 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+using APIWorkmate.Context;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string mySqlConnection = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection,
+    ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build();
 
